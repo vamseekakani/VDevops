60cd31d53715886813b571f0f06044746721644a
vamsi kakani <vamsee.kakani@outlook.com>
2019-11-28 20:46:56 +0100

    first commit

---
+++ src/main/java/com/intelliqit/App.java
@@ -0,0 +1,13 @@
+package com.intelliqit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
